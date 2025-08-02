4139d3b727911e746da5be103c767fb88d7667ba
mahir-blueIoT <mahir@blueiot.com>
2025-08-02 11:20:14 +1000

    vercel deployment changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,12 @@
-
-
-import React from 'react'
-import LoginForm from '../../components/forms/AuthForm'
-import AuthForm from '../../components/forms/AuthForm'
+import AuthForm from "../../components/forms/AuthForm";
+import { Suspense } from "react";
 
 export default function Login() {
   return (
-    <div className='flex items-center justify-center h-screen w-screen'>
-        <AuthForm/>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthForm />
+      </Suspense>
     </div>
-  )
+  );
 }
